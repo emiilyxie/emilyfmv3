4619e889a1883fd8191536d49cc91cf41a3ba2e2
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-08-04 23:22:13 -0700

    added navbar styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,14 +3,14 @@ import styles from './navbar.module.css'
 
 export default function Navbar() {
   return (
-    <ul className={styles.navbar}> 
-      <li>
+    <ul className={styles.navContainer}> 
+      <li className={styles.navItem}>
         <Link href="/">Home</Link>
       </li>
-      <li>
+      <li className={styles.navItem}>
         <Link href="/about">About</Link>
       </li>
-      <li>
+      <li className={styles.navItem}>
         <Link href="/resume">Resume</Link>
       </li>
     </ul>
