39716ec52283b66548a098bd79f6d9eca0ded1b3
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-08-06 23:40:20 -0700

    add expand button to each project item

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styles from './projects.module.css'
 
 export type ProjectType = {
@@ -6,14 +7,23 @@ export type ProjectType = {
 };
 
 export function ProjectItem(props: { project: ProjectType }) {
+
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  let toggleExpanded = () => {
+    setIsExpanded(!isExpanded)
+  }
+
   return (
     <div>
       <div className={styles.projectHeading}>
         {/* TODO: get out of className hell */}
         <h3 className={styles.projectHeadingText}>{props.project.title}</h3>
-        <span className={styles.projectHeadingButton}>+</span>
+        <button onClick={toggleExpanded} className={styles.projectHeadingButton}>{isExpanded ? "-" : "+"}</button>
       </div>
+      {isExpanded &&
       <p className={styles.projectDescription}>{props.project.description}</p>
+      }
     </div>
   )
 }
@@ -22,7 +32,7 @@ export function ProjectSection(props: { title: string, projects: ProjectType[] }
   return (
     <div>
       <div>
-        <h2 className={styles.projectSectionHeader}>{props.title}</h2>
+        <h2 className={styles.projectSectionHeader}>{`(${props.title})`}</h2>
       </div>
 
       {props.projects.map((project) => (
