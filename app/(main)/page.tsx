fe85f3c4069cc1067c8f6f74b34070864e3027fa
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-08-10 11:07:07 -0700

    remove extra entries in project list

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -34,44 +34,12 @@ export default function Home(props: { projectSections: {title: string, projects:
           {
             "title": "UCSF",
             "description": "Researching the shape of the meniscus."
-          },
-          {
-            "title": "NASA Jet Propulsion Laboratory",
-            "description": "Developed planning software for Europa Clipper, a mission launching for Jupiter in 2024."
-          },
-          {
-            "title": "Augmented Perception Lab",
-            "description": "Exploring sound perception in virtual reality."
-          },
-          {
-            "title": "UCSF",
-            "description": "Researching the shape of the meniscus."
-          },
-          {
-            "title": "NASA Jet Propulsion Laboratory",
-            "description": "Developed planning software for Europa Clipper, a mission launching for Jupiter in 2024."
-          },
-          {
-            "title": "Augmented Perception Lab",
-            "description": "Exploring sound perception in virtual reality."
-          },
-          {
-            "title": "UCSF",
-            "description": "Researching the shape of the meniscus."
           }
         ]
       },
       {
         "title": "Projects",
         "projects": [
-          {
-            "title": "JazzCats",
-            "description": "i love u"
-          },
-          {
-            "title": "JazzCats",
-            "description": "i love u"
-          },
           {
             "title": "JazzCats",
             "description": "i love u"
