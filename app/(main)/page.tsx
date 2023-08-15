82295c99d0412a055999226c4e453b38dc3fd20b
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-08-14 17:29:21 -0700

    imported project data from json file

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,58 +1,21 @@
 'use client'
 import { ProjectType, ProjectSection } from "@/components/projects"
-import { getProjectsData } from "@/lib/localdata"
 import styles from "./page.module.css"
+import projectData from "../../json/projects.json"
 
 /*
 TODOs:
-- add link to projects
-- make resume page link to pdf
 - add dropdown animations
-- add hover effects
-- fix flexbox stuff
-- add scroll interaction?
 - fill in content
 */
 
 export default function Home(props: { projectSections: {title: string, projects: ProjectType[]}[] }) {
 
-  // TODO: put the data in a local json file
-  const projectSections = [
-      {
-        "title": "Work",
-        "projects": [
-          {
-            "title": "NASA Jet Propulsion Laboratory",
-            "description": "Developed planning software for Europa Clipper, a mission launching for Jupiter in 2024.",
-            "link": "https://www.jpl.nasa.gov/missions/europa-clipper/"
-          },
-          {
-            "title": "Augmented Perception Lab",
-            "description": "Exploring sound perception in virtual reality.",
-            "link": "/about"
-          },
-          {
-            "title": "UCSF",
-            "description": "Researching the shape of the meniscus."
-          }
-        ]
-      },
-      {
-        "title": "Projects",
-        "projects": [
-          {
-            "title": "JazzCats",
-            "description": "i love u"
-          }
-        ]
-      }
-    ]
-
   return (
     <>
         <p className={styles.blurb}>hi i'm emily, an undergraduate at cmu majoring in information systems and computer science. cs is my ultimate passion. four square and seven years ago.</p>
         {
-          projectSections.map((projectSection) => (
+          projectData.projectSections.map((projectSection) => (
             <ProjectSection
               key={projectSection.title}
               title={projectSection.title}
