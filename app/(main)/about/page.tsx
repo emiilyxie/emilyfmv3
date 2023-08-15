1f05e67b4f2d73f8b6cea1e14a802a574987c8f6
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-08-15 00:31:39 -0700

    spacing formatting

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -7,6 +7,7 @@ export default function About() {
         Hi! I'm Emily Xie, an undergraduate at CMU studying Information Systems and Computer Science. I love coding applications, both practical and for fun. My current technological interests include AI/ML, web development, and computer graphics.<br></br><br></br>In my free time, I generally listen to jazz albums or draw on my tablet.
       </p>
       <br></br>
+      <br></br>
       <div style={{"position": "relative"}}>
         <Image 
           src="/img/headshot.jpg" 
